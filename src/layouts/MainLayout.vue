82b968d2c5315b2f2023053033b7ca6b308a73ec
Pearcee <peshokii99@gmail.com>
2024-11-03 10:52:57 +0700

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HeaderContent from '@/components/HeaderContent.vue'
+import FooterContent from '@/components/FooterContent.vue'
+</script>
+
+<template>
+  <HeaderContent />
+  <main>
+    <slot></slot>
+  </main>
+  <FooterContent />
+</template>
